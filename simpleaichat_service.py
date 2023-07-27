f4f1fdaed6d0508fefef2d8ed0652717f57d652e
Venkatesh Chandrasekharan <venkatesh.cv@gmail.com>
2023-07-27 21:15:04 +0530

    tools and google api based search

--- simpleaichat_service.py
+++ simpleaichat_service.py
@@ -17,11 +17,15 @@ def init_azure_openai(response_instructions:str = "you are an AI assistant. You 
     return ai
 
 
-def q_and_a(response_instructions:str = "you are an AI assistant. You will provide helpful answers", queries:list[str] = []) -> None:
+def q_and_a(response_instructions:str = "you are an AI assistant. You will provide helpful answers", queries:list[str] = []) -> list[str]:
     ai = init_azure_openai(response_instructions)
+    responses = []
     for query in queries: 
         response = ai(query)
         print(response)
+        responses.append(response)
+    return responses
+    
 
 
 def q_and_a_stream(response_instructions:str = "you are an AI assistant. You will provide helpful answers", queries:list[str] = []) -> None:
@@ -65,6 +69,7 @@ def google_search_serpapi(query:str):
 
 def google_search(query:str):
     """search the internet using google"""
+    print("searching for %s " % query)
     results = search(query, advanced=True)
     return ",".join(list(map(lambda result: result.description, results)))
 
@@ -77,12 +82,33 @@ def wiki_lookup(query:str):
 
 def q_and_a_with_tools(response_instructions:str = "You are an AI assistant. You will answer queries in a helpful manner", queries:list[str]=[]) -> None:
     ai = init_azure_openai()
-    for query in queries:
-        response = ai(system= response_instructions, prompt=query, tools=[google_search, wiki_lookup], save_messages=True)
+    contextualized_queries = q_and_a(response_instructions= "You  are an AI assistant. You will respond with a rephrased prompt with additional context only if needed. Else respond with the original prompt", 
+             queries=queries)
+    for query in contextualized_queries:
+        response = ai(system= response_instructions, prompt=query, tools=[google_api_search, wiki_lookup], save_messages=True)
         print(query)
         print(response['response'])
         print("tool used %s" % response['tool'])
 
+
+def google_api_search(query:str):
+    """search the internet"""
+    import requests  
+    api_key = "AIzaSyCG64sq0bc1my-8KEYKOgunwErBl3bAOQw"  
+    search_query = query 
+    programmable_search_engine_id = "c67702f624fca43c1"
+    url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={programmable_search_engine_id}&q={search_query}"  
+    response = requests.get(url)  
+  
+    if response.status_code == 200:  
+        data = response.json()  
+        items = data['items']
+        snippets = list(map(lambda item: item['snippet'], items))
+        # Process the response data as per your requirements  
+        joined_snippets = ",".join(snippets)
+        return joined_snippets
+    else:  
+        print("Error:", response.status_code)  
 
 # write a main function that calls demo
 if __name__ == "__main__":
@@ -91,7 +117,11 @@ if __name__ == "__main__":
     # q_and_a_stream(response_instructions= "You  are an AI assistant. You will help write emails", 
     #         queries=["write a 100 word email to all employees on the importance of respectfulness to colleagues"])
     # prime_database()
+    # print('database primed')
     # q_and_a_with_context(response_instructions= "you are an AI assistant. You will answer queries in one sentence.", queries=["where does peter knowles work","does he have children"], tags=["marvel"])
     # q_and_a(response_instructions="{'country':'India', 'headOfState':'Narendra Modi', 'population': '1.3 billion'}", queries=["norway", "iceland"])
-    # q_and_a_with_output_schema(output_schema=get_event_metadata, queries=["when the Berlin wall fell", "when did the USSR dissolve","when did communist government fall in Poland"])
-    q_and_a_with_tools(response_instructions= "You  are an AI assistant. You will answer queries in one sentence", queries=["what is the maximum temperature in Chennai, India today","who is kamala Harris", "where was she, Kamala Harris, born", "when was she born", "What are her educational qualifications"])
+    q_and_a_with_output_schema(output_schema=get_event_metadata, queries=["when the Berlin wall fall", "when did the USSR dissolve","when did communist government fall in Poland"])
+    #q_and_a_with_tools(response_instructions= "You  are an AI assistant. You will answer queries in one sentence", queries=["what is the maximum temperature in chennai, India today","who is kamala Harris","what is the maximum temperature in Watertown,Boston, USA today", "where was she born", "when was she born", "What are her educational qualifications", "for which football club does lionel messi play for now in 2023"])
+    # q_and_a(response_instructions= "You  are an AI assistant. You will respond with a rephrased prompt with additional context only if needed. Else respond with the original prompt", 
+    #         queries=["what is the maximum temperature in Chennai, India today","who is kamala Harris","what is the maximum temperature in kuala lumpur, Malaysia today", "where was she born", "when was she born", "What are her educational qualifications", "which club does lionel messi play for now in 2023"])
+    # google_api_search()
